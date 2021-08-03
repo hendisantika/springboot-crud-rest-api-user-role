1ffd87bbb15cd46e8183c358c28911a1c0e8240e
Hendi Santika <hendisantika@gmail.com>
2021-08-04 06:18:27 +0700

    #16 - Add WebSecurityConfig

---
+++ src/main/java/com/hendisantika/userrole/config/WebSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.hendisantika.userrole.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-crud-rest-api-user-role
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/08/21
+ * Time: 06.17
+ */
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+}
