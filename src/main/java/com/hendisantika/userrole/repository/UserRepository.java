8da1df9dea3739e5f037a057c9fdd24be53297ad
Hendi Santika <hendisantika@gmail.com>
2021-08-04 06:09:21 +0700

    #7 - Add User Repository

---
+++ src/main/java/com/hendisantika/userrole/repository/UserRepository.java
@@ -0,0 +1,22 @@
+package com.hendisantika.userrole.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-crud-rest-api-user-role
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/08/21
+ * Time: 06.07
+ */
+public interface UserRepository extends JpaRepository<User, Long> {
+    @Query("SELECT u FROM User u WHERE u.email = ?1")
+    User findByEmail(String email);
+
+    @Query("SELECT u FROM User u WHERE u.email = ?1 or u.username = ?2")
+    User findByEmailOrUsername(String email, String username);
+
+}
