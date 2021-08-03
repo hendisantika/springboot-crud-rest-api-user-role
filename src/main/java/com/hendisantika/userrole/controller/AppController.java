c8f9c74593dbb99bbfd6ab18e0f9de973f5a0532
Hendi Santika <hendisantika@gmail.com>
2021-08-04 06:22:32 +0700

    #22 - Add processRegister in AppController

--- src/main/java/com/hendisantika/userrole/controller/AppController.java
+++ src/main/java/com/hendisantika/userrole/controller/AppController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -28,10 +29,17 @@ public class AppController {
         return "index";
     }
 
-    @GetMapping("/register")
+    @GetMapping("register")
     public String showRegistrationForm(Model model) {
         model.addAttribute("user", new User());
 
         return "signup_form";
+    }
+
+    @PostMapping("process_register")
+    public String processRegister(User user) {
+        service.registerDefaultUser(user);
+
+        return "register_success";
     }
 }
