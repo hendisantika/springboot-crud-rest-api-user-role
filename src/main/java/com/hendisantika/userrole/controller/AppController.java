da278d18939da96cf781cfd5145d6823a17fd73d
Hendi Santika <hendisantika@gmail.com>
2021-08-04 06:20:44 +0700

    #19 - Add AppController

---
+++ src/main/java/com/hendisantika/userrole/controller/AppController.java
@@ -0,0 +1,22 @@
+package com.hendisantika.userrole.controller;
+
+import com.hendisantika.userrole.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-crud-rest-api-user-role
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/08/21
+ * Time: 06.20
+ */
+@Controller
+public class AppController {
+
+    @Autowired
+    private UserService service;
+
+}
