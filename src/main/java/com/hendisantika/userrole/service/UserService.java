74d0c24ccdd159cecdd6fdb07503fe04dbb15370
Hendi Santika <hendisantika@gmail.com>
2021-08-04 06:10:30 +0700

    #8 - Add User Service

---
+++ src/main/java/com/hendisantika/userrole/service/UserService.java
@@ -0,0 +1,29 @@
+package com.hendisantika.userrole.service;
+
+import com.hendisantika.userrole.repository.RoleRepository;
+import com.hendisantika.userrole.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-crud-rest-api-user-role
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/08/21
+ * Time: 06.09
+ */
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private RoleRepository roleRepo;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+}
