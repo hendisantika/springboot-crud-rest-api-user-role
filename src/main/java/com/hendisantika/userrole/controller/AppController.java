c2721eb1090a7204a1382ea3ce83c13557871dfe
Hendi Santika <hendisantika@gmail.com>
2021-08-04 06:23:39 +0700

    #24 - Add editUser in AppController

--- src/main/java/com/hendisantika/userrole/controller/AppController.java
+++ src/main/java/com/hendisantika/userrole/controller/AppController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -51,5 +52,14 @@ public class AppController {
         model.addAttribute("listUsers", listUsers);
 
         return "users";
+    }
+
+    @GetMapping("users/edit/{id}")
+    public String editUser(@PathVariable("id") Long id, Model model) {
+        User user = service.get(id);
+        List<Role> listRoles = service.listRoles();
+        model.addAttribute("user", user);
+        model.addAttribute("listRoles", listRoles);
+        return "user_form";
     }
 }
