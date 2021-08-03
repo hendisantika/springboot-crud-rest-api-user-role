1583dc689aa39224fac03ccbd4c67cd965a9a49c
Hendi Santika <hendisantika@gmail.com>
2021-08-04 06:07:08 +0700

    #6 - Add Role Repository

---
+++ src/main/java/com/hendisantika/userrole/repository/RoleRepository.java
@@ -0,0 +1,18 @@
+package com.hendisantika.userrole.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-crud-rest-api-user-role
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 04/08/21
+ * Time: 06.05
+ */
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+    @Query("SELECT r FROM Role r WHERE r.name = ?1")
+    Role findByName(String name);
+}
