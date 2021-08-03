df650b311d03c07627a49fb4d8dea68c9ebd306d
Hendi Santika <hendisantika@gmail.com>
2021-08-04 06:21:57 +0700

    #21 - Add showRegistrationForm in AppController

--- src/main/java/com/hendisantika/userrole/controller/AppController.java
+++ src/main/java/com/hendisantika/userrole/controller/AppController.java
@@ -3,6 +3,7 @@ package com.hendisantika.userrole.controller;
 import com.hendisantika.userrole.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -27,4 +28,10 @@ public class AppController {
         return "index";
     }
 
+    @GetMapping("/register")
+    public String showRegistrationForm(Model model) {
+        model.addAttribute("user", new User());
+
+        return "signup_form";
+    }
 }
