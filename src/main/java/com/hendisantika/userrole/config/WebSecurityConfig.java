ae23c7f6b627ccf4e5e83a08095427ae2f594c08
Hendi Santika <hendisantika@gmail.com>
2021-08-04 11:51:59 +0700

    #37 - Still error while adding new user on API User

--- src/main/java/com/hendisantika/userrole/config/WebSecurityConfig.java
+++ src/main/java/com/hendisantika/userrole/config/WebSecurityConfig.java
@@ -51,6 +51,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().and().csrf().disable()
                 .httpBasic()
                 .and()
                 .authorizeRequests()
